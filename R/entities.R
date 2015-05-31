6e0b5c582c740f19cb932f94c11841e5c847a8ff
ironholds <ironholds@gmail.com>
2015-05-31 19:56:40 -0400

    add export tags

--- R/entities.R
+++ R/entities.R
@@ -1,4 +1,6 @@
 #'@title Access Descriptions of Structures in PDB Files
+#'
+#'@export
 get_structure_descriptions <- function(structure_ids, ...){
   structure_ids <- format_multiple(structure_ids)
   url <- paste0("describeMol?structureId=", structure_ids)
@@ -6,6 +8,8 @@ get_structure_descriptions <- function(structure_ids, ...){
 }
 
 #'@title Access Descriptions of Chemicals in PDB Files
+#'
+#'@export
 get_chemical_descriptions <- function(chemical_ids, ...){
   chemical_ids <- format_multiple(chemical_ids)
   url <- paste0("describeMol?chemicalID=", chemical_ids)
