d1f6efaa60d1950bc1ab0cd0658483b9d1aaed51
ironholds <ironholds@gmail.com>
2015-05-31 19:19:48 -0400

    add

--- R/files.R
+++ R/files.R
@@ -37,7 +37,30 @@ list_pdbs <- function(obsolete = FALSE, ...){
 #'@description \code{get_pdb_description} allows you to
 #'access metadata about a particular PDB file, including what structures it
 #'contains, when it was released, and what resolution the underlying data has.
-#'For obsolete files, the metadata is instead the ID of the file that replaced
+#'
+#'@param file_ids a character vector of file IDs. If you don't have them to hand,
+#'these can be retrieved with \code{\link{list_pdbs}}.
+#'
+#'@param ... further arguments to pass to httr's GET.
+#'
+#'@examples
+#'
+#'#Return metadata about PDB files
+#'pdb_metadata <- get_pdb_description(c("116L","100D","14PS"))
+#'
+#'@export
+get_pdb_description <- function(file_ids, ...){
+
+  #Check and format
+  file_ids <- format_multiple(file_ids)
+  url <- paste0("describePDB?structureId=", file_ids)
+  return(pdb_query(url, ...))
+}
+
+#'@title Access Information about Entities within PDB Files
+#'@description \code{get_pdb_entites} retrieves data about the
+#'content of particular PDB files, and the structures within them.
+#'For obsolete files, this is largely the ID of the file that replaced
 #'it.
 #'
 #'@param file_ids a character vector of file IDs. If you don't have them to hand,
@@ -47,11 +70,11 @@ list_pdbs <- function(obsolete = FALSE, ...){
 #'
 #'@examples
 #'
-#'#Return metadata about obsolete and non-obsolete PDB files
-#'pdb_metadata <- get_pdb_description(c("116L","100D","14PS"))
+#'#Return metadata about the entities within obsolete and non-obsolete PDB files
+#'pdb_metadata <- get_pdb_entities(c("116L","100D","14PS"))
 #'
 #'@export
-get_pdb_description <- function(file_ids, ...){
+get_pdb_entities <- function(file_ids, ...){
 
   #Check and format
   file_ids <- format_multiple(file_ids)
